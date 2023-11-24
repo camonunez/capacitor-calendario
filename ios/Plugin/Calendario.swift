21f687a5ab424699502794ab796323cf9def76d4
Camilo Núñez <camonunez@gmail.com>
2023-11-23 22:28:05 -0300

    ios: luego de crear evento abre el calendario en esa fecha.

--- ios/Plugin/Calendario.swift
+++ ios/Plugin/Calendario.swift
@@ -1,5 +1,6 @@
 import Foundation
 import EventKit
+import EventKitUI
 
 @objc public class Calendario: NSObject {
     @objc public func echo(_ value: String) -> String {
@@ -7,53 +8,58 @@ import EventKit
         return value
     }
 
-    @objc public func crearEvento(titulo: String, descripcion: String, unixInicio: Double, unixFin: Double, ubicacion: String) -> Bool {
-        print(titulo)
-        print(descripcion)
-        print(unixInicio)
-        print(unixFin)
-        print(ubicacion)
+    @objc public func crearEvento(titulo: String, descripcion: String, unixInicio: Double, unixFin: Double, ubicacion: String) -> EKEvent {
+        print("titulo", titulo)
+        print("descripcion", descripcion)
+        print("unixInicio", unixInicio)
+        print("unixFin", unixFin)
+        print("ubicacion", ubicacion)
 
         // Usar EventKit para crear el evento
         // https://developer.apple.com/documentation/eventkit/ekeventstore
 
-        let eventStore = EKEventStore()
 
         var resultado = false
-        eventStore.requestAccess(to: .event) { (granted, error) in
-            print("granted \(granted)")
-            print("error \(String(describing: error))")
-            if (!granted) {
-                // call.reject([error: "sinPermiso"])
-                print("sinPermiso")
-                resultado = false
-            } else if (error != nil) {
-                // call.reject([error: "error"])
-                print("error")
-                resultado = false
-            } else {
-                let event = EKEvent(eventStore: eventStore)
-                event.title = titulo
-                event.startDate = Date(timeIntervalSince1970: unixInicio)
-                event.endDate = Date(timeIntervalSince1970: unixFin)
-                event.notes = descripcion
-                event.location = ubicacion
-                event.calendar = eventStore.defaultCalendarForNewEvents
+        
+        let eventStore = EKEventStore()
+        let evento = EKEvent(eventStore: eventStore)
+        evento.title = titulo
+        evento.startDate = Date(timeIntervalSince1970: unixInicio)
+        evento.endDate = Date(timeIntervalSince1970: unixFin)
+        evento.notes = descripcion
+        evento.location = ubicacion
+        evento.calendar = eventStore.defaultCalendarForNewEvents
+        
+        
 
-                do {
-                    try eventStore.save(event, span: .thisEvent)
-                    print("Evento guardado", event)
-                    // call.resolve([
-                    //     "resultado": true
-                    // ])
-                    resultado = true
-                } catch let error as NSError {
-                    print("guardadoFallido error : \(error)")
-                    // call.reject([error: "guardadoFallido"])
-                    resultado = false
+        Task {
+            do {
+                // Intenta solicitar acceso a los eventos del calendario
+                let accessGranted = try await eventStore.requestAccess(to: .event)
+
+                if accessGranted {
+                    print("Acceso concedido a eventos del calendario.")
+                    // Puedes realizar operaciones relacionadas con eventos aquí
+                    
+                    try eventStore.save(evento, span: .thisEvent)
+                    print("Evento guardado", evento, "\n")
+                    
+                    // Crea una URL para abrir la aplicación de Calendario con el evento específico
+                    if evento.eventIdentifier != nil,
+                       let calendarURL = URL(string: "calshow:\(evento.startDate.timeIntervalSinceReferenceDate)") {
+                        
+                        // Abre la aplicación de Calendario
+                        await UIApplication.shared.open(calendarURL, options: [:], completionHandler: nil)
+                    }
+                } else {
+                    print("Acceso denegado a eventos del calendario.")
                 }
+            } catch {
+                // Manejar cualquier error que pueda ocurrir durante la solicitud de acceso
+                print("Error al solicitar acceso: \(error)")
             }
         }
-        return resultado
+
+        return evento
     }
 }
